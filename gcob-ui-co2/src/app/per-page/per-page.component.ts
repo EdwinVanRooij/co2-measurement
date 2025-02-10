0d111238ebb9fdbc3d670d163fd279912dc45b85
Edwin van Rooij <edwinvrooij@gmail.com>
2025-02-10 16:29:38 +0100

    updates

--- gcob-ui-co2/src/app/per-page/per-page.component.ts
+++ gcob-ui-co2/src/app/per-page/per-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { co2 } from '@tgwf/co2';
-import { readFileSync } from 'fs';
 import { convert } from 'pagexray';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-per-page',
@@ -11,21 +11,23 @@ import { convert } from 'pagexray';
   styleUrl: './per-page.component.scss'
 })
 export class PerPageComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
-    this.getCo2EmissionPerPage('../har-files/dashboard.har')
+    this.getCo2EmissionPerPage();
   }
 
-  getCo2EmissionPerPage(filePath: string): any{
-    const swd =  new co2({ model: "swd", version: 4 });
-    const oneByte = new co2({ model: "1byte" });
-    
-    const filesString = readFileSync(filePath, 'utf-8');
+  getCo2EmissionPerPage(): void {
+    this.http.get('assets/har-files/dashboard.har', { responseType: 'text' })
+        .subscribe(filesString => {
+          const har = JSON.parse(filesString);
+          const page = convert(har);
 
-    const har = JSON.parse(filesString);
-    const page = convert(har);
+          const swd = new co2({ model: 'swd', version: 4 });
+          const oneByte = new co2({ model: '1byte' });
 
-    const emissions_swd_per_page = swd.perPage(page, true)
-    const emissions_1byte_per_page = oneByte.perPage(page, true)
-
+          const emissions_swd_per_page = swd.perPage(page, true);
+          const emissions_1byte_per_page = oneByte.perPage(page, true);
+        });
   }
 }
