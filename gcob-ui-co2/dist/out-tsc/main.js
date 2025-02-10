c64934dbbe9f5d89ae4c8417eedd25b0569623fd
Edwin van Rooij <edwinvrooij@gmail.com>
2025-02-10 16:09:57 +0100

    reinit gitignore

--- gcob-ui-co2/dist/out-tsc/main.js
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config.js';
-import { AppComponent } from './app/app.component.js';
-bootstrapApplication(AppComponent, appConfig)
-    .catch((err) => console.error(err));
-//# sourceMappingURL=main.js.map
